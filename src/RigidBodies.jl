bd7e91251fa3320abce53510f60d198edb35ef76
Leonardo Igler <leonardo.igler@tum.de>
2022-11-15 16:08:38 +0100

    Newest update

--- src/RigidBodies.jl
+++ src/RigidBodies.jl
@@ -112,7 +112,7 @@ function rigid_body_simple(torque_B, force_B, x_W_0, v_B_0, R_W_0, ω_B_0, t_0, 
 
     # --------- Translation ------------------------------------
     #gravity = [0.0, 0.0, -9.81]
-    gravity = [0.0, 0.0, 0]
+    gravity = [0.0, 0.0, 0.0]
     dv_W = (R_W_0 * force_B + (mass .* gravity)) .* (Δt / mass)
     v_W_0 = R_W_0 * v_B_0 # transform Body Velocity in World frame
     v_W_1 = v_W_0 + dv_W # integrate velocity
